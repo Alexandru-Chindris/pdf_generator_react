74ac2df14f5b98cc9cf97b7a7c63f3f940d9ffc3
Chindris Alexandru <alexandruioan.chindris@istitutoagnelli.it>
2022-12-11 16:43:23 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import { jsPDF } from "jspdf";
 
 function App() {
   const[date, setDate] = useState('')
-  const[doc, setDoc] = useState('')
+  const[docc, setDoc] = useState('')
   const[name, setName] = useState('')
   const[address, setAddress] = useState('')
   const[piva, setPiva] = useState('')
@@ -12,6 +13,7 @@ function App() {
   const[articleId, setArticleId] = useState('')
   const[qtn, setQtn] = useState('')
   const[price, setPrice] = useState('')
+  const[fileName, setFileName] = useState('')
 
   const handleDate = event => {
     setDate(event.target.value)
@@ -53,7 +55,11 @@ function App() {
     setPrice(event.target.value)
   };
 
-  const handleSubmit = event => {
+  const handleFileName = event =>{
+    setFileName(event.target.value)
+  };
+// Test function
+ /*  const handleSubmit = event => {
     event.preventDefault();
     alert(`Dati inseriti: \n 
             DataFattura: ${date} \n 
@@ -67,46 +73,75 @@ function App() {
             Contenuto
             NomeArt: ${articleId} \n
             Qta: ${qtn} \n
-            PrezzoUnitario: ${price} \n`);
-  };
+            PrezzoUnitario: ${price} \n
+            `);
+  }; */
+
+  function handlePDF() {
+    const doc = new jsPDF();
+
+    // timestamp
+    const now = new Date();
+    doc.text(20,20, `Generated at: ${now.getDay()}/${now.getMonth()}/${now.getFullYear()}`);
+
+    //data insertion
+    doc.text(`Date: ${date}`,20,40 );
+    doc.text(`Document number: ${docc}`,20,60 );
+    doc.text(`Name: ${name}`,20,80 );
+    doc.text(`Address: ${address}`,20,100 );
+    doc.text(`iban: ${piva}`,20,120 );
+    doc.text(`Fiscal ID: ${fiscal}`,20,140 );
+    doc.text(`Unique ID: ${uniqueId}`,20,160 );
+    doc.text(`Article name:${articleId}`,20,180 );
+    doc.text(`Amount: ${qtn}`,20,200 );
+    doc.text(`Price: ${price}`,20,220 );
+
+    doc.save(fileName+".pdf"); 
+  }
 
   return (
     <div>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handlePDF} class="container">
           <h3>Dati generali</h3>
           <div>
-            <input type="date" required="required" value={date} onChange={handleDate}/>
+            <input class="input" type="date" required="required" value={date} onChange={handleDate}/>
           </div>
-          <div>
-          <input type="number" placeholder="Inserisci il numero della fattura" required="required" value={doc} onChange={handleDoc}/>
+          <div >
+          <input class="input" type="number" placeholder="insert docoment number" required="required" value={docc} onChange={handleDoc}/>
           </div>
           <h3>Informazioni destinatario</h3>
           <div>
-            <input type="text" placeholder="Inserisci il tuo nome" required="required" value={name} onChange={handleName}/>
+            <input class="input" type="text" placeholder="Insert your name" required="required" value={name} onChange={handleName}/>
           </div>
           <div>
-            <input type="text" placeholder="Inserisci il tuo inidirizzo" required="required" value={address} onChange={handleAddress}/>
+            <input class="input" type="text" placeholder="Insert your address" required="required" value={address} onChange={handleAddress}/>
           </div>
           <div>
-            <input type="text" placeholder="Inserisci il PIVA" required="required" value={piva} onChange={handlePiva}/>
+            <input class="input" type="text" placeholder="Insert your IBAN" required="required" value={piva} onChange={handlePiva}/>
           </div>
           <div>
-            <input type="text" placeholder="Inserisci il tuo codice fiscale" required="required" value={fiscal} onChange={handleFiscal}/>
+            <input class="input" type="text" placeholder="Insert your fiscal id" required="required" value={fiscal} onChange={handleFiscal}/>
           </div>
           <div>
-            <input type="text" placeholder="Inserisci il codice univoco" required="required" value={uniqueId} onChange={handleUniqueId}/>
-          </div>
-          <h3>Contenuto</h3>
-          <div>
-            <input type="text" placeholder="nome articolo" required="required" value={articleId} onChange={handleArticleId}/>
+            <input class="input" type="text" placeholder="insert your unique id" required="required" value={uniqueId} onChange={handleUniqueId}/>
           </div>
           <div>
-            <input type="number" placeholder="quantita" required="required" value={qtn} onChange={handleQtn}/>
+          <h3>Content</h3>
+          <div>
+            <input class="input" type="text" placeholder="article name" required="required" value={articleId} onChange={handleArticleId}/>
           </div>
           <div>
-            <input type="number" step="any" placeholder="prezzo unitario" required="required" value={price} onChange={handlePrice}/>
+            <input class="input" type="number" placeholder="amount" required="required" value={qtn} onChange={handleQtn}/>
           </div>
-          <button type="submit">
+          <div>
+            <input class="input" type="number" step="any" placeholder="price" required="required" value={price} onChange={handlePrice}/>
+          </div>
+        </div>
+        <h3>Additional information for generating PDF</h3>
+          <div>
+            <input class="input" type="text" placeholder="pdf name" required="required" value={fileName} onChange={handleFileName}/>
+          </div>
+          <button type='submit' class="submit">
             Crea PDF
           </button>
         </form>
